a09079454a10a5332746d375c350f804a8f7ba1f
Yauheni <basillion@yandex.ru>
2022-05-18 16:18:53 +0300

    2.0002

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 
-echo "<h1>Hello from jenkins v2.0001</h1>";
+echo "<h1>Hello from jenkins v2.0002</h1>";
 echo "<h2>Server IP is $ip_server</h2>";
-echo "<h2>build # $BUILD_ID</h2>";
+echo "<h2>builded through pipeline</h2>";
 ?> 
