baf0734517270322d7f48e84088dba12213bc2cc
Yauheni <basillion@yandex.ru>
2022-05-19 12:14:57 +0300

    2.0003

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 $ip_server = $_SERVER['SERVER_ADDR'];
 
-echo "<h1>Hello from jenkins v2.0002</h1>";
+echo "<h1>Hello from jenkins v2.0003</h1>";
 echo "<h2>Server IP is $ip_server</h2>";
-echo "<h2>builded through pipeline</h2>";
+echo "<h2>Polling test</h2>";
 ?> 
